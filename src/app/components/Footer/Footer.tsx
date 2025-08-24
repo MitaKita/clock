fac31dfec30fc37c0ad6b15c3b88bdd9067563e7
MitaKita <marita_kontio@hotmail.com>
2025-08-24 18:35:56 -0300

    Styling changed

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-[#222] text-white py-4 text-center mt-auto">
+  <footer className="bg-cyan-900/90 text-white py-4 text-center mt-auto">
     <span>&copy; maritakontio.com</span>
   </footer>
 );
